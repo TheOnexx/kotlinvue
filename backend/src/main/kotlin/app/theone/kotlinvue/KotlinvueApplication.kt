435891abb0022f49273c2053d88c190e9486e09e
TheOnexx <theoneneov@gmail.com>
2019-11-24 23:10:53 -0500

    Init commit

---
+++ backend/src/main/kotlin/app/theone/kotlinvue/KotlinvueApplication.kt
@@ -0,0 +1,11 @@
+package app.theone.kotlinvue
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinvueApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinvueApplication>(*args)
+}
---
+++ backend/src/test/kotlin/app/theone/kotlinvue/KotlinvueApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.theone.kotlinvue
+
+import org.junit.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinvueApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
