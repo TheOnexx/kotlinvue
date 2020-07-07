c2252e95617cc1dc61b95ddcb091ad873cb408ee
TheOnexx <theoneneov@gmail.com>
2020-07-07 12:23:13 -0400

    Products table init

--- frontend/src/components/admin/AdminProducts.vue
+++ frontend/src/components/admin/AdminProducts.vue
@@ -1,6 +1,29 @@
 <template>
     <div>
         Products!
+
+        <table class="centered">
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Category</th>
+                <th>Price</th>
+            </tr>
+            <tr v-for="product in products" v-bind:key="product.id">
+                <td>
+                    {{product.name}}
+                </td>
+                <td>
+                    {{product.description}}
+                </td>
+                <td>
+                    {{product.category}}
+                </td>
+                <td>
+                    {{product.price}}
+                </td>
+            </tr>
+        </table>
     </div>
 </template>
 
@@ -11,11 +34,21 @@
         name: "AdminProducts",
 
         data: () => ({
-            products: []
+            products: [],
+            errorMsg: ''
         }),
 
         created() {
-            AXIOS.get('/')
+            AXIOS.get('/products/all')
+                .then(response => {
+                    this.products = response.data
+                },
+                e => {
+                    this.errorMsg = e.response.data.message
+                })
+                .catch(e => {
+                    this.errorMsg = e.response.data.message
+                })
         }
     }
 </script>
